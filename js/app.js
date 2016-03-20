ae669506053bbac9bc7bf1312b8d50746bc92068
Divya van Mahajan <dvm@vanmahajan.com>
2016-03-20 12:21:03 -0700

    Firebase logging enabled

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+
+var messagesRef = new Firebase('https://intense-torch-3200.firebaseio.com/xcmonitoring/set1');
 var querydate=moment();
 var querydatetext="";
 var historyStats={};
@@ -64,10 +66,17 @@ function getObjectsChanged(object_name,name_field) {
     var changed = document.getElementById(object_name+"_changed");
     var changed2 = document.getElementById(object_name+"_summary");
     var query2 = 'select COUNT() from '+object_name+' where LastModifiedDate > '+date.format();
+    var tsRef = messagesRef.child(querydatetext);
+    var updateMsg = {};
+    updateMsg[object_name]=-1;
+    tsRef.update(updateMsg);    
     force.query(query2, function (response) {
         try {
-            console.log("Count succeeded.");
+            // console.log("Count succeeded.");
             historyStats[querydatetext][object_name]=response.totalSize;
+            updateMsg[object_name]=response.totalSize;
+            tsRef.update(updateMsg);
+            // console.info(updateMsg);  
             if (changed!=null) {
                 changed.innerHTML = response.totalSize +' '+ object_name+ " changes.";            
             }
@@ -84,8 +93,15 @@ function getObjectsChanged(object_name,name_field) {
 
 function refreshData() {
     querydate=moment().subtract(intervalMinutes,'m');
-    querydatetext = querydate.format();// "dddd, MMMM Do YYYY, h:mm:ss a");
+    try {
+        querydatetext = querydate.toISOString();// "2016-03-19T03:50:55.591Z"
+        querydatetext = querydatetext.replace(/.[0-9][0-9][0-9]Z/,"Z"); // Strip out microseconds
+    } catch(e) {
+        console.error('No toISOString() '+e);
+    }
     historyStats[querydatetext]={'Contact':-1,'Case':-1,'ServiceContract':-1};
+    var tsRef = messagesRef.child(querydatetext);
+    tsRef.set({'Contact':-1,'Case':-1,'ServiceContract':-1,'WCContact':-1,'WCServiceContract':-1,'WCCase':-1,'Delta':-1}); 
     showDate();
     showToken();
     getObjectsChanged('Contact','Name');
@@ -130,6 +146,54 @@ function showHistory() {
     document.getElementById('history_table').innerHTML = str;
 
 }
+var savedrows = {};
+function showFirebaseHistory(snapshot) {
+    //GET DATA
+    var stamp = snapshot.key();
+    var data = snapshot.val();
+    // console.info(JSON.stringify(data));
+    var columns = '<td>-</td>' +
+            '<td>' + stamp + '</td>' +
+            '<td>' + data['Contact'] + '</td>' +
+            '<td>' + data['Case'] + '</td>' +
+            '<td>' + data['ServiceContract'] + '</td>' +
+            '<td>' + data['WCContact'] + '</td>' +
+            '<td>' + data['WCCase'] + '</td>' +
+            '<td>' + data['WCServiceContract'] + '</td>' +
+            '<td>' + data['Delta'] + '</td>';
+    
+    // Using JQuery to add a row to the table
+    var row = savedrows[stamp];
+    //console.info('1'+"#"+stamp);
+    if (!row)
+    {
+        var str = "<tr/>";
+        row=$(str);
+        row.html(columns);
+        var tblbody = $('#firebase_table');
+        tblbody.append(row);
+        savedrows[stamp]=row;
+        //console.info('2');
+    } else {
+        //console.info('3');
+        row.html(columns);
+        
+    }
+    
+}
+function removeFirebaseHistory(snapshot)
+{
+    //GET DATA
+    var stamp = snapshot.key();
+    var data = snapshot.val();
+    console.info('remove - '+stamp);
+    var row = savedrows[stamp];
+    if (row) {
+        savedrows[stamp]=undefined;
+        row.remove();
+    }
+}
+
 function autoShowHistory()
 {
     showHistory();
@@ -146,4 +210,8 @@ if (window.location.hostname=='salty-gorge-66919.herokuapp.com') {
 }
 // login();
 
+var query = messagesRef.limitToLast(10);
+query.on('child_added',showFirebaseHistory);
+query.on('child_changed',showFirebaseHistory);
+query.on('child_removed',removeFirebaseHistory);
 
